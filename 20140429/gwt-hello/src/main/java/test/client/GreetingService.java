0a0810f0ed3b770b831efe33140c148b3c067e8a
Neeraj Verma <neeraj.verma.ports@vermatech.com>
2016-02-14 16:55:58 -0500

    add

---
+++ 20140429/gwt-hello/src/main/java/test/client/GreetingService.java
@@ -0,0 +1,9 @@
+package test.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("greet")
+public interface GreetingService extends RemoteService {
+	String greetServer(String input) throws IllegalArgumentException;
+}
---
+++ 20140429/gwt-hello/src/main/java/test/client/GreetingServiceAsync.java
@@ -0,0 +1,9 @@
+package test.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface GreetingServiceAsync {
+
+	void greetServer(String input, AsyncCallback<String> callback);
+
+}
