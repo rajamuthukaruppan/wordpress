269ffd1ee3504cc34b4176a024252a52231375b9
Neeraj Verma <neeraj.verma.ports@vermatech.com>
2016-02-14 16:54:42 -0500

    gwt

--- 20140429/gwt-hello/src/main/java/test/client/Matrix.java
+++ 20140429/gwt-hello/src/main/java/test/client/Matrix.java
@@ -1,21 +1,42 @@
 package test.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
- 
+
+/**
+ * to run: mvn clean compile gwt:run
+ */
 public class Matrix implements EntryPoint {
- 
-    public void onModuleLoad() {
-        Button button = new Button("Send", new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                Window.alert("Hello World!");
-            }
-        });
-        button.setStyleName("sendButton");
-        RootPanel.get("sendButtonContainer").add(button);
-    }
+	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+
+	@Override
+	public void onModuleLoad() {
+		Button button = new Button("Send", new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				Window.alert("Hello World!");
+				greetingService.greetServer("World", new AsyncCallback<String>() {
+					@Override
+					public void onFailure(Throwable caught) {
+						// Show the RPC error message to the user
+						Window.alert("Remote Procedure Call - Failure");
+					}
+
+					@Override
+					public void onSuccess(String result) {
+						Window.alert(result);
+					}
+				});
+
+			}
+		});
+		button.setStyleName("sendButton");
+		RootPanel.get("sendButtonContainer").add(button);
+	}
 }
