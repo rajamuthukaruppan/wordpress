e0f8af631e58199f2ffc02e26bc7136986d64def
Neeraj Verma <neeraj.verma.ports@vermatech.com>
2016-02-17 21:49:34 -0500

    small fixes

---
+++ 20160215/JSFTest/src/main/java/com/test/AddressBeanRequestScoped.java
@@ -0,0 +1,73 @@
+package com.test;
+ 
+import java.io.Serializable;
+ 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+ 
+@ManagedBean
+@RequestScoped
+public class AddressBeanRequestScoped implements Serializable {
+ 
+    private static final long serialVersionUID = 1L;
+     
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+ 
+    @PostConstruct
+    void init() {
+        System.out.println("Initializing " + this.getClass().getName());
+    }
+     
+    @PreDestroy
+    void destroy() {
+        System.out.println("Destroying " + this.getClass().getName());
+    }
+ 
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void handleKeyEvent() {
+        address = address.toUpperCase();
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }   
+}
