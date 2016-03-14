d856d84211b9c94bbfedfcf7324571af487cda06
Neeraj Verma <neeraj.verma.ports@vermatech.com>
2016-03-14 01:20:53 -0400

    removed reference to unmanaged stuff.

--- 20160313/UserManagerApp/src/test/java/org/test/ContainerProducer.java
+++ 20160313/UserManagerApp/src/test/java/org/test/ContainerProducer.java
@@ -11,13 +11,9 @@ public class ContainerProducer {
 	    // Create the container instance, passing it the properties map:
 	    Map<String, Object> p = new HashMap<>();
 	    String userHome = System.getProperty("user.home");
-        p.put("jta-managed", "new://Resource?type=DataSource");
-        p.put("jta-managed.JdbcUrl", "jdbc:hsqldb:file:"+userHome+"/data/"+dbName+";shutdown=true");
+        p.put("java:/DefaultDS", "new://Resource?type=DataSource");
+        p.put("java:/DefaultDS.JdbcUrl", "jdbc:hsqldb:file:"+userHome+"/data/"+dbName+";shutdown=true");
 
-        p.put("un-managed", "new://Resource?type=DataSource");
-        p.put("un-managed.JdbcUrl", "jdbc:hsqldb:file:"+userHome+"/data/"+dbName+";shutdown=true");
-        p.put("un-managed.JtaManaged", "false");
-        
 	    return javax.ejb.embeddable.EJBContainer.createEJBContainer(p);
 	}
 }
