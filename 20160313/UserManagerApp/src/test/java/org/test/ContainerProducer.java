f231773c5a9ffd2e2c9d760a54d4337367cf69a9
Neeraj Verma <neeraj.verma.ports@vermatech.com>
2016-03-14 20:04:04 -0400

    openebj does not close connections that are specified in the jndi format.

--- 20160313/UserManagerApp/src/test/java/org/test/ContainerProducer.java
+++ 20160313/UserManagerApp/src/test/java/org/test/ContainerProducer.java
@@ -11,8 +11,8 @@ public class ContainerProducer {
 	    // Create the container instance, passing it the properties map:
 	    Map<String, Object> p = new HashMap<>();
 	    String userHome = System.getProperty("user.home");
-        p.put("java:/DefaultDS", "new://Resource?type=DataSource");
-        p.put("java:/DefaultDS.JdbcUrl", "jdbc:hsqldb:file:"+userHome+"/data/"+dbName+";shutdown=true");
+        p.put("DefaultDS", "new://Resource?type=DataSource");
+        p.put("DefaultDS.JdbcUrl", "jdbc:hsqldb:file:"+userHome+"/data/"+dbName+";shutdown=true");
 
 	    return javax.ejb.embeddable.EJBContainer.createEJBContainer(p);
 	}
