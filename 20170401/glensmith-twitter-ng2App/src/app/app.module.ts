9c160a9e56ea76d4eccb929472aee6bd2d406ef8
Neeraj Verma <numberformat@gmail.com>
2017-04-05 21:45:18 -0400

    Day 17

--- 20170401/glensmith-twitter-ng2App/src/app/app.module.ts
+++ 20170401/glensmith-twitter-ng2App/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { FriendsComponent } from './friends/friends.component';
 import {routing, appRoutingProviders } from './app.routing';
 import { FriendComponent } from './friend/friend.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FriendComponent } from './friend/friend.component';
     FeedComponent,
     MessagesComponent,
     FriendsComponent,
-    FriendComponent
+    FriendComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- 20170401/glensmith-twitter-ng2App/src/app/app.routing.ts
+++ 20170401/glensmith-twitter-ng2App/src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FeedComponent } from './feed/feed.component';
+import { LoginComponent } from './login/login.component';
 import { FriendsComponent } from './friends/friends.component';
 import { FriendComponent } from './friend/friend.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -8,6 +9,7 @@ import { MessagesComponent } from './messages/messages.component';
 const appRoutes: Routes = [
   { path: '',  redirectTo: '/feed',  pathMatch: 'full'},
   { path: 'feed', component: FeedComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'friends', component: FriendsComponent },
   { path: 'messages', component: MessagesComponent },
   { path: 'friends/:friendId', component: FriendComponent}
