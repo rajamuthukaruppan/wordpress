a6ff4b78a01902ed1496afaac852d20c29cfe3f9
Neeraj Verma <numberformat@gmail.com>
2017-04-25 23:40:57 -0400

    breadcrumbs, message display, save and cancel on customer page

--- 20170418/northwindApp/src/app/customer/customer.component.ts
+++ 20170418/northwindApp/src/app/customer/customer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Customer } from '../customer';
 import { ActivatedRoute } from '@angular/router';
 import { CustomerService } from '../customer.service';
+import { Location } from '@angular/common';
 import 'rxjs/Rx';
 
 @Component({
@@ -12,17 +13,22 @@ import 'rxjs/Rx';
 export class CustomerComponent implements OnInit {
 
   customer : Customer;
+  customerOrig : Customer;
   customerId = '';  
   errorText = '';
   loaded = false;
   firstName = 'test';
+  message = "";
+  
 
-  constructor(private route : ActivatedRoute, private customerService : CustomerService) { }
+  constructor(private route : ActivatedRoute, private customerService : CustomerService,
+  private location: Location) { }
 
   ngOnInit() {
   this.route.params.map(params => params['customerId']).subscribe((customerId) => {
     this.customerService.getCustomerById(customerId).subscribe((customer) => {
       this.customer = customer;
+      this.customerOrig = JSON.parse(JSON.stringify(customer));
       console.log(this.customer);
     }, (error) => {
       this.errorText = error;
@@ -32,9 +38,21 @@ export class CustomerComponent implements OnInit {
   });
   }
 
-  public onSubmit(form) {    
+  public onSubmit(form) {
+    if(!form.dirty) {
+      this.message = "Nothing to save";
+      return;
+    }
     this.customerService.saveCustomer(form.value).subscribe((customer) => {
-      console.log("put completed", customer);
+      this.message="Changes saved";      
+    }, (error) => {
+      this.errorText = error;
+    }, () => {
+      this.loaded = true;
     });
   }
+
+    onCancel() {
+        this.location.back();
+    }  
 }
